7b1f3cb80b89cac6e31d8eb490b522f5479345ed
lin <451846939@qq.com>
2025-04-17 22:06:28 +0800

    remove log

--- arceos/modules/axhal/src/platform/riscv64_qemu_virt/misc.rs
+++ arceos/modules/axhal/src/platform/riscv64_qemu_virt/misc.rs
@@ -1,8 +1,8 @@
-use axlog::ax_println;
+
 
 /// Shutdown the whole system, including all CPUs.
 pub fn terminate() -> ! {
-    ax_println!("Shutting down...");
+    info!("Shutting down...");
     sbi_rt::system_reset(sbi_rt::Shutdown, sbi_rt::NoReason);
     warn!("It should shutdown!");
     loop {
